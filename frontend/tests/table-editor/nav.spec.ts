ab68f4304af53b384e4376e97393e8a1d7249a07
egor <58992960+egor-romanov@users.noreply.github.com>
2023-05-11 17:25:21 +0300

    add frontend tests (#40)
    * add a few examples tests

---
+++ frontend/tests/table-editor/nav.spec.ts
@@ -0,0 +1,107 @@
+import { expect } from '@playwright/test'
+import { test } from '../../src/execution/persistent-ctx'
+
+const dashboardUrl = process.env.SUPA_DASHBOARD || 'https://app.supabase.com'
+const projectId = process.env.FE_TESTS_PROJECT_REF || 'aaaaaaaaaaaaaaaaaaaa'
+const editorUrl = `${dashboardUrl}/project/${projectId}/editor`
+
+test('editor opens', async ({ page }) => {
+  await page.goto(editorUrl)
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/frontend-tests/)
+})
+
+test('select schema', async ({ page }) => {
+  await page.goto(editorUrl)
+
+  await page.isEnabled('"public"')
+  await page.click('"schema"')
+  await page.hover('"auth"')
+  await page.click('"auth"', { delay: 50 })
+
+  await page.click('nav >> "users"')
+  await expect(page.getByRole('columnheader').first()).toBeVisible()
+})
+
+test('search table', async ({ page }) => {
+  await page.goto(editorUrl)
+
+  await page.getByPlaceholder('Search tables').type('staff', { delay: 20 })
+
+  const menu = await page.$('nav')
+  const staffMenuItems = (await menu?.$$('"staff"'))?.length
+  const menuItems = (await menu?.$$('a'))?.length
+  expect(staffMenuItems).toBe(menuItems)
+
+  await page.click('"staff"', { delay: 50 })
+  await expect(page.getByRole('columnheader').first()).toBeVisible()
+})
+
+test('search view', async ({ page }) => {
+  await page.goto(editorUrl)
+
+  await page.getByPlaceholder('Search tables').type('actor_info', { delay: 20 })
+
+  const menu = await page.$('nav')
+  const viewMenuItems = (await menu?.$$('"actor_info"'))?.length
+  const menuItems = (await menu?.$$('a'))?.length
+  expect(viewMenuItems).toBe(menuItems)
+
+  await page.click('"actor_info"', { delay: 50 })
+  await expect(page.getByRole('columnheader').first()).toBeVisible()
+})
+
+// todo: rewrite to be atomic by deleting via API in the create test, and creating via API in the delete test
+test.describe.serial('table operations', () => {
+  const tableName = 'test_table_' + Date.now()
+  test('create simple table', async ({ page }) => {
+    await page.goto(editorUrl)
+    await page.click('"New table"', { delay: 50 })
+
+    await page.getByText(/Create a new table under/).isVisible()
+    await page.type('input:right-of(:text("Name"))', tableName, { delay: 20 })
+
+    await page.click('"Save"', { delay: 50 })
+    const resp = await page.waitForResponse((r) => {
+      return r.url().includes('tables?id=') && r.request().method() === 'GET'
+    })
+    expect(resp.ok()).toBeTruthy()
+  })
+
+  // todo: add test for search bug: search for table -> click on the result => search input is cleared
+  // todo: remove this once the search bug is fixed
+  test('delete table workaround', async ({ page }) => {
+    await page.goto(editorUrl)
+    await page.getByPlaceholder('Search tables').type(tableName, { delay: 20 })
+    await page.click(`"${tableName}"`, { delay: 50 })
+
+    // this is needed because of the search bug if you immediately click on table
+    await page.getByPlaceholder('Search tables').type(tableName, { delay: 20 })
+
+    await page.click(`button:right-of(:text("${tableName}"))`)
+    await page.click('"Delete Table"', { delay: 50 })
+    await page.click('"Delete"', { delay: 50 })
+
+    const resp = await page.waitForResponse((r) => {
+      return r.url().includes('tables?id=') && r.request().method() === 'DELETE'
+    })
+    expect(resp.ok()).toBeTruthy()
+  })
+
+  // todo: enable this once the search bug is fixed
+  test.fixme('delete table', async ({ page }) => {
+    await page.goto(editorUrl)
+    await page.getByPlaceholder('Search tables').type(tableName, { delay: 20 })
+    await page.click(`"${tableName}"`, { delay: 50 })
+
+    await page.click(`button:right-of(:text("${tableName}"))`)
+    await page.click('"Delete Table"', { delay: 50 })
+    await page.click('"Delete"', { delay: 50 })
+
+    const resp = await page.waitForResponse((r) => {
+      return r.url().includes('tables?id=') && r.request().method() === 'DELETE'
+    })
+    expect(resp.ok()).toBeTruthy()
+  })
+})
---
+++ frontend/tests/table-editor/view-controls.spec.ts
@@ -0,0 +1,63 @@
+import { expect } from '@playwright/test'
+import { test } from '../../src/execution/persistent-ctx'
+
+const dashboardUrl = process.env.SUPA_DASHBOARD || 'https://app.supabase.com'
+const projectId = process.env.FE_TESTS_PROJECT_REF || 'aaaaaaaaaaaaaaaaaaaa'
+const editorUrl = `${dashboardUrl}/project/${projectId}/editor`
+
+const actorInfoViewId = process.env.FE_TESTS_ACTOR_INFO_VIEW_ID || '0'
+const actorInfoViewUrl = `${editorUrl}/${actorInfoViewId}`
+
+test('insert not available on view', async ({ page }) => {
+  await page.goto(actorInfoViewUrl)
+
+  // Expect a insert button to not be visible after table is loaded
+  await expect(page.getByRole('columnheader').first()).toBeVisible()
+  await expect(page.getByText('"Insert"')).not.toBeVisible()
+})
+
+test('API docs are available on view', async ({ page }) => {
+  await page.goto(actorInfoViewUrl)
+
+  // Expect an API button to not be visible after table is loaded
+  await expect(page.getByRole('columnheader').first()).toBeVisible()
+  await expect(page.getByText('API')).toBeVisible()
+
+  await page.click('"API"')
+  await expect(page.getByText('header').getByText('API')).toBeVisible()
+})
+
+test('open view definition', async ({ page }) => {
+  await page.goto(actorInfoViewUrl)
+
+  // Expect an API button to not be visible after table is loaded
+  await expect(page.getByRole('columnheader').first()).toBeVisible()
+  await expect(page.getByText('Definition')).toBeVisible()
+
+  await page.click('"definition"')
+  await expect(page.getByText('SQL Definition of actor_info')).toBeVisible()
+  await expect(page.getByText('create view')).toBeVisible()
+  await expect(page.getByText('public.actor_info as')).toBeVisible()
+})
+
+test('filter view by id', async ({ page }) => {
+  // id of actor_info view in supabase
+  await page.goto(actorInfoViewUrl)
+
+  await expect(page.getByRole('columnheader').first()).toBeVisible()
+
+  await page.click('"Filter"', { delay: 50 })
+  await page.click('"Add filter"', { delay: 50 })
+  await page.getByPlaceholder('Enter a value').type('4', { delay: 20 })
+  await page.click('"Apply filter"', { delay: 50 })
+  await page.click('.sb-grid')
+
+  const resp = await page.waitForResponse(/query\?key=public-actor_info/)
+  expect(resp.ok()).toBe(true)
+  await expect(page.getByText('Loading records count...')).not.toBeVisible()
+
+  const filteredRows = await page.$$('.rdg-row')
+  expect(filteredRows.length).toBe(1)
+  const filteredIds = await filteredRows[0].$$eval('.rdg-cell', (cells) => cells[1].textContent)
+  expect(filteredIds).toBe('4')
+})
