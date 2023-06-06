683be96f02e36886496cf2b42324a07843378cd7
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-06-06 14:18:46 +0200

    struggling with the focus on monaco editor and running it

--- frontend/tests/sql-editor/nav.spec.ts
+++ frontend/tests/sql-editor/nav.spec.ts
@@ -27,19 +27,89 @@ test('SQL editor opens and can click on new query 2', async ({ page }) => {
   const gettingStartedLoaded = await page.getByText(/Getting started/)
   await expect(gettingStartedLoaded !== undefined).toBeTruthy()
 
-  const userContentResp = await page.waitForResponse((r) => {
-    return r.url().includes('/content') && r.request().method() === 'GET'
+  // wait for API calls to finish
+  //   const userContentResp = await page.waitForResponse((r) => {
+  //     return r.url().includes('/content') && r.request().method() === 'GET'
+  //   })
+  //   const userPermissionsResp = await page.waitForResponse((r) => {
+  //     return r.url().includes('/permissions') && r.request().method() === 'GET'
+  //   })
+  const entityDefinitions = await page.waitForResponse((r) => {
+    return r.url().includes('/query?key=entity-definitions') && r.request().method() === 'POST'
   })
-  await expect(userContentResp.ok()).toBeTruthy()
+  //   await expect(userContentResp.ok()).toBeTruthy()
+  //   await expect(userPermissionsResp.ok()).toBeTruthy()
+  await expect(entityDefinitions.ok()).toBeTruthy()
 
-  await page.click('"New query"')
+  // create first snippet
+  await await page.click('"New query"')
+
+  const snippetOne = await page.url()
+  console.log('first snippet', snippetOne)
+
+  await page.goto(editorUrl)
+  await page.goto(snippetOne)
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/SQL | Supabase/)
 
+  //
+  //
   // should check that monaco editor is visible.
   //   await page.waitForSelector('.monaco-editor')
-  // check that text in the results section is visible.
-  //   const resultsDefaultText = await page.getByText(/Click RUN to execute your query./)
-  //   await expect(resultsDefaultText !== undefined).toBeTruthy()
+  //   check that text in the results section is visible.
+  const resultsDefaultText = await page.getByText(/Click RUN to execute your query./)
+  await expect(resultsDefaultText !== undefined).toBeTruthy()
+
+  // Wait for Monaco Editor to be ready
+  await page.waitForSelector('.monaco-editor')
+
+  // Get the Monaco Editor instance
+  const monacoEditor = await page.$('.monaco-editor')
+
+  if (!monacoEditor) {
+    console.error('Monaco Editor element not found')
+    return
+  }
+
+  const editorContent = await monacoEditor.$('div[role="code"]')
+
+  if (!editorContent) {
+    console.error('Editor content element not found')
+    return
+  }
+
+  // Focus the Monaco Editor
+  await editorContent.focus()
+
+  // Emulate typing using low-level key events
+  await page.keyboard.press('Meta+A') // Select all existing text
+  await page.keyboard.press('Backspace') // Delete the selected text
+  await page.keyboard.type('Hello, Monaco Editor!') // Type the desired text
+  //
+  //
+  //
+  //
+  //
+  //
+
+  // Determine the appropriate key for CMD or Control
+  const cmdKey = process.platform === 'darwin' ? 'Meta' : 'Control'
+
+  // Simulate CMD+Enter keyboard shortcut
+  await page.keyboard.down(cmdKey)
+  await page.keyboard.press('Enter')
+  await page.keyboard.up('Enter')
+  await page.keyboard.up(cmdKey)
+
+  const firstSqlQueryResp = await page.waitForResponse((r) => {
+    return r.url().includes('/query') && r.request().method() === 'POST'
+  })
+  await expect(firstSqlQueryResp.ok()).toBeTruthy()
+
+  await expect(page.getByText(/query should not be empty/)).toBeVisible()
+  await expect(page.getByText(/Invalid SQL query/)).toBeVisible()
+
+  // create first snippet
+  //   await await page.click('"run"')
 })
