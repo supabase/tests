137ac418b76670ad95913160542807a95443605d
egor <58992960+egor-romanov@users.noreply.github.com>
2024-01-31 09:17:24 +0400

    Update timeoutFetch.ts

--- src/common/timeoutFetch.ts
+++ src/common/timeoutFetch.ts
@@ -6,10 +6,8 @@ export default async function fetch(
   init?: RequestInit,
   timeout: number = 10000
 ): Promise<Response> {
-  const abort = new AbortController()
-  if (init) init.signal = abort.signal
   if (init?.method === 'POST' && timeout === 10000) {
     timeout = 15000
   }
-  return timeoutPromise(crossFetch(input, init), timeout, abort)
+  return timeoutPromise(crossFetch(input, init), timeout)
 }
